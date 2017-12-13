af640d58d271d52723eca4a2d117582628fe108e
Michael Duffy <duffym9@go.stockton.edu>
2017-12-12 19:47:39 -0500

    Added Angular

---
+++ MvcMovie/MvcMovie/ClientApp/app/components/movies/movies.component.ts
@@ -0,0 +1,25 @@
+﻿import { Component, Inject } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Component({
+    selector: 'movies',
+    templateUrl: './movies.component.html',
+})
+export class MoviesComponent {
+
+    public movies: Movie[];
+
+    constructor(http: Http, @Inject('BASE_URL') baseUrl: string) {
+        http.get(baseUrl + 'api/Movies').subscribe(result => {
+            this.movies = result.json() as Movie[];
+        }, error => console.error(error));
+    }
+}
+
+interface Movie {
+    ID: number;
+    Title: string;
+    Price: number;
+    Genre: string;
+    Rating: string;
+}
---
+++ MvcMovie/MvcMovie/Controllers/API/MovieController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers.API
+{
+    [Produces("application/json")]
+    [Route("api/Movie")]
+    public class MovieController : Controller
+    {
+
+
+
+
+    }
+}
